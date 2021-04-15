5911955cdff0a71b3923bb23ebbb13dd35f10623
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2021-04-15 17:15:45 -0600

    pieces transform changed

--- Assets/Scripts/GamePieces.cs
+++ Assets/Scripts/GamePieces.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class GamePieces : MonoBehaviour
 {
+    GameObject[] gameObjects;
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
